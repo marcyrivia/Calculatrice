2c1a5205b19e60c09d8fff86ae979098b44fe091
Shamsddine <goumdissshamsddine@gmail.com>
2023-11-28 12:53:19 +0100

    fini

--- script.js
+++ script.js
@@ -1,28 +1,34 @@
 let inputBox = document.querySelector("#inputBox")
-let button = document.querySelectorAll("button")
+let buttons = document.querySelectorAll("button")
 let string = ""
+let oprClicked = false; // Nouvelle variable pour suivre si un opr a été cliqué
 
-button.forEach(element => {
-  element.addEventListener("click", (b) =>{
-    if(b.target.innerText == "="){
+buttons.forEach(button => {
+  button.addEventListener("click", (event) => {
+    const buttonText = event.target.innerText;
+
+    if (buttonText === "=") {
       string = String(eval(string))
       inputBox.value = string
-    }
-    else if (b.target.innerText == "AC"){
+      oprClicked = false; // Réinitialise la variable après le calcul
+    } else if (buttonText === "AC") {
       string = ""
       inputBox.value = string
-    }
-    else if (b.target.innerText == "DEL"){
+      oprClicked = false; // Réinitialise la variable
+    } else if (buttonText === "DEL") {
       string = string.substring(0, string.length - 1)
       inputBox.value = string
-    }
-    else if (b.target.innerText == "plusMinus"){
+      oprClicked = false; // Réinitialise la variable
+    } else if (event.target.id === "plusMinus") {
       string = String(-eval(string))
       inputBox.value = string
-    }
-    else{
-      string += b.target.innerText
+    } else if (event.target.classList.contains("opr") && !oprClicked) {
+      string += buttonText
+      inputBox.value = string
+      oprClicked = true; // Désactive les opr après un clic
+    } else if (!event.target.classList.contains("opr")) {
+      string += buttonText
       inputBox.value = string
     }
-  })
+  });
 });
