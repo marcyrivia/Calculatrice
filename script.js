c4e0a0517cc95982398419cd5f6382a141a031cc
Shamsddine <goumdissshamsddine@gmail.com>
2023-11-29 14:44:28 +0100

    changement de theme fini

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ let buttons = document.querySelectorAll("button")
 let string = ""
 let oprClicked = false; // Nouvelle variable pour suivre si un opr a été cliqué
 let switchs = document.querySelector("#icon")
+const secretCode = "23152631619"
+
 
 buttons.forEach(button => {
   button.addEventListener("click", (event) => {
@@ -30,41 +32,70 @@ buttons.forEach(button => {
     } else if (!event.target.classList.contains("opr")) {
       string += buttonText
       inputBox.value = string
+    } else if (inputBox.value = secretCode){
+
     }
   });
 });
 
+
+
+
+
 var body = document.body;
 var container = document.querySelector(".container");
 var chiffres = document.querySelectorAll(".chiffre");
+let oprs = document.querySelectorAll(".opr");
+let deletes = document.querySelectorAll(".delete"); // Ajout de la sélection des boutons avec la classe 'delete'
+
 
 switchs.addEventListener("click", function() {
-  // Vérifiez si l'élément a la première classe
+  // Vérifie si l'icône a la première classe
   if (switchs.classList.contains("bi-brightness-high-fill")) {
-    // Si oui, supprimez la première classe et ajoutez la deuxième classe
+    // Si oui, supprime la première classe et ajoute la deuxième classe
     switchs.classList.remove("bi-brightness-high-fill");
     switchs.classList.add("bi-moon-fill");
     switchs.style.color = "black";
     container.style.backgroundColor = "#fff";
-    body.style.backgroundColor = "#222225"; // Replace "yourDesiredColor" with the color you want
+    body.style.backgroundColor = "#222225"; // Remplacez "votreCouleurSouhaitée" par la couleur que vous souhaitez
 
-    // Iterate through each chiffre and set the background color
+    // Parcours chaque chiffre et définis la couleur de fond
     chiffres.forEach(function(chiffre) {
       chiffre.style.backgroundColor = "white";
       chiffre.style.color = "#222225";
     });
+
+    // Parcours chaque opr et définis la couleur de fond en blanc
+    oprs.forEach(function(opr) {
+      opr.style.backgroundColor = "white";
+    });
+
+    // Parcours chaque delete et définis la couleur de fond en blanc
+    deletes.forEach(function(deleteBtn) {
+      deleteBtn.style.backgroundColor = "white";
+    });
   } else {
-    // Sinon, ajoutez la première classe et supprimez la deuxième classe
+    // Sinon, ajoute la première classe et supprime la deuxième classe
     switchs.classList.add("bi-brightness-high-fill");
     switchs.classList.remove("bi-moon-fill");
-    body.style.backgroundColor = "initial";
+    body.style.backgroundColor = "white";
     switchs.style.color = "white";
     container.style.backgroundColor = "#222225";
 
-    // Iterate through each chiffre and set the background color back to default
+    // Parcours chaque chiffre et rétablis la couleur de fond par défaut
     chiffres.forEach(function(chiffre) {
-      chiffre.style.backgroundColor = "initial";
+      chiffre.style.backgroundColor = "#222225";
       chiffre.style.color = "white";
+    });
+
+    // Parcours chaque opr et rétablis la couleur de fond à la valeur initiale
+    oprs.forEach(function(opr) {
+      opr.style.backgroundColor = "#222225";
+    });
+
+    // Parcours chaque delete et rétablis la couleur de fond à la valeur initiale
+    deletes.forEach(function(deleteBtn) {
+      deleteBtn.style.backgroundColor = "#222225";
     });
   }
 });
